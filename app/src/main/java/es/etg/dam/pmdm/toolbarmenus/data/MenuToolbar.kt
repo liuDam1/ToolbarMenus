16bcf11e36b2fd0d1a0a68a265228457152fb593
liuDam1 <liu.jiangyu.dam@gmail.com>
2025-11-18 17:40:59 +0100

    Terminado con Dinamicos

---
+++ app/src/main/java/es/etg/dam/pmdm/toolbarmenus/data/MenuToolbar.kt
@@ -0,0 +1,7 @@
+package es.etg.dam.pmdm.toolbarmenus.data
+
+import es.etg.dam.pmdm.toolbarmenus.R
+
+object MenuToolbar {
+    val MENUS =arrayOf (R.string.MENU_OPC_ABRIR, R.string.MENU_OPC_CERRAR, R.string.MENU_OPC_3, R.string.MENU_OPC_4 )
+}
--- app/src/main/java/es/etg/dam/pmdm/toolbarmenus/view/MainActivity.kt
+++ app/src/main/java/es/etg/dam/pmdm/toolbarmenus/view/MainActivity.kt
@@ -1,4 +1,4 @@
-package es.etg.dam.pmdm.toolbarmenus
+package es.etg.dam.pmdm.toolbarmenus.view
 
 import android.os.Bundle
 import android.view.Menu
@@ -7,6 +7,8 @@ import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.appcompat.widget.Toolbar
+import es.etg.dam.pmdm.toolbarmenus.R
+import es.etg.dam.pmdm.toolbarmenus.data.MenuToolbar
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -20,21 +22,41 @@ class MainActivity : AppCompatActivity() {
         setSupportActionBar(toolbar)
     }
 
+//    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+//        menuInflater.inflate(R.menu.menu_main, menu)
+//        return true;
+//    }
+
     override fun onCreateOptionsMenu(menu: Menu?): Boolean {
-        menuInflater.inflate(R.menu.menu_main, menu)
+        super.onCreateOptionsMenu(menu)
+        if(menu!=null) {
+
+            for ((index, valor) in MenuToolbar.MENUS.withIndex()) {
+                menu.add(Menu.NONE, index , Menu.NONE, valor)
+            }
+        }
         return true;
     }
 
+//    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+//        if(item.itemId == R.id.id_navegar){
+//            navegar();
+//            return true;
+//        }
+//        return super.onOptionsItemSelected(item);
+//    }
+
     override fun onOptionsItemSelected(item: MenuItem): Boolean {
-        if(item.itemId == R.id.id_navegar){
+        if(item.toString() == getString(R.string.MENU_OPC_CERRAR)){
             navegar();
             return true;
         }
+
         return super.onOptionsItemSelected(item);
     }
 
     private fun navegar() {
         val text = "Has seleccionado Navegar"
-        val toast = Toast.makeText(this, text,Toast.LENGTH_SHORT).show()
+        val toast = Toast.makeText(this, text, Toast.LENGTH_SHORT).show()
     }
 }
